40cea6a5ddcdf6f4fedfbd39645181824cc72d35
Amit Thapa <amithapa1994@gmail.com>
2022-05-26 08:41:15 +0530

    Invalid Type.

--- calc/functions.py
+++ calc/functions.py
@@ -1,4 +1,10 @@
-def add(num1, num2):
+from typing import Union
+from .utils import is_numeric
+
+def add(num1: Union[int, float], num2: Union[int, float]):
+    if not is_numeric(num1) or not is_numeric(num2):
+        raise TypeError("Invalid type")
+        
     return num1 + num2
 
 def sub(num1, num2):
---
+++ calc/utils.py
@@ -0,0 +1,4 @@
+def is_numeric(x):
+    if isinstance(x, (int, float)):
+        return True
+    return False
